a153c72075618d03ead496702af8a81f1b1d08c6
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-18 19:33:21 +0900

    added states in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import Login from './components/Login';
+import EndGame from './components/EndGame';
+import Navbar from './components/Navbar';
 import Game from './components/Game';
 
 const App = () => {
+  const [ showLogin, setShowLogin ] = useState(true);
+  const [ showEndGame, setShowEndGame ] = useState(false);
+  const [ name, setName ] = useState("");
+  const [ score, setScore ] = useState(0);
+
   return (
     <div>
+      <Login />
+      <EndGame />
+      <Navbar />
       <Game />
     </div>
   )
