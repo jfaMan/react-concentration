0cf16491f77dd2eb320402bdcf63a59694dd872f
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-22 00:23:08 +0900

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ const Navbar = (props) => {
   return (
     <div className="navbar">
       <img src={logo} alt="Logo" />
-      <div className="game-title">BANJO-KAZOOIE CONCENTRATION</div>
       <div className="name">PLAYER NAME: {name}</div>
       <div className="score">SCORE: {score}</div>
     </div>
