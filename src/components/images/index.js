3c7a09e0d6169b1ae3787d5fcfa6cdcd921e31b0
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-21 21:45:09 +0900

    created index.js of images

---
+++ src/components/images/index.js
@@ -0,0 +1,51 @@
+import Banjo from './Banjo.png';
+import Kazooie from './Kazooie.png';
+import Bottles from './Bottles.png';
+import Gruntilda from './Gruntilda';
+import Jiggy from './Jiggy.png';
+import Jinjo from './Jinjo.png';
+import Mumbo from './Mumbo.png';
+import Rareware from './Rareware.png';
+
+export let images = [
+  {
+    name: "Banjo",
+    pic: Banjo,
+    flipped: false,
+  },
+  {
+    name: "Kazooie",
+    pic: Kazooie,
+    flipped: false,
+  },
+  {
+    name: "Bottles",
+    pic: Bottles,
+    flipped: false,
+  },
+  {
+    name: "Gruntilda",
+    pic: Gruntilda,
+    flipped: false,
+  },
+  {
+    name: "Jiggy",
+    pic: Jiggy,
+    flipped: false,
+  },
+  {
+    name: "Jinjo",
+    pic: Jinjo,
+    flipped: false,
+  },
+  {
+    name: "Mumbo",
+    pic: Mumbo,
+    flipped: false,
+  },
+  {
+    name: "Rareware",
+    pic: Rareware,
+    flipped: false,
+  },
+]
