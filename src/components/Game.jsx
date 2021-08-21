1b0305f185b2ea5a25f0ed5f1c33de5caf5e914f
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-22 02:05:14 +0900

    added logo to background

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Images from './Images'
+import Logo from './images/Logo.png'
 
 const Game = () => {
   return (
     <div className='game' style={{ display: 'none' }}>
+      <img className="bounce" src={Logo} alt="Logo" />
       <Images />
     </div>
   )
