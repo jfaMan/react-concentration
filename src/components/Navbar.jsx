0a7246ad6a0c5ce87dbb717a0f5b4a44705a5aca
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-26 00:54:45 +0900

    changed filenames case to be consistent otherwise deploy fail

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './images/Navbar.png';
+import NavbarLogo from './images/NavbarLogo.png';
 
 const Navbar = (props) => {
   const { name, score } = props;
   return (
     <div className="navbar">
-      <img src={logo} alt="Logo" />
+      <img src={NavbarLogo} alt="Logo" />
       <div className="score">TRIES: {score}</div>
       <div className="name">PLAYER NAME: {name}</div>
     </div>
