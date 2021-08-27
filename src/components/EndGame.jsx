7f069833d33286061262cad13c2e5b47d7624205
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-27 22:05:31 +0900

    made end game message typewriter each line correctly

--- src/components/EndGame.jsx
+++ src/components/EndGame.jsx
@@ -3,14 +3,36 @@ import Banjo from './images/EndGameBanjo.gif';
 import Gruntilda from './images/EndGameGruntilda.gif';
 import BanjoVoice from './music/BanjoVoice.mp3';
 import GruntildaVoice from './music/GruntildaVoice.mp3';
+// import Typewriter from 'typewriter-effect';
 
 const EndGame = (props) => {
   const {playerName, score, endGame, refreshImages} = props;
   const [ banjo ] = useState(new Audio(BanjoVoice));
   const [ gruntilda ] = useState(new Audio(GruntildaVoice));
+  const [ finalScore ] = useState(score * 10)
+  const [ messageWinA, setMessageWinA ] = useState();
+  const [ messageWinB, setMessageWinB ] = useState();
+  const [ messageLoseA, setMessageLoseA ] = useState();
+  const [ messageLoseB, setMessageLoseB ] = useState();
 
   useEffect(() => {
-    score !== 0 ? banjo.play() : gruntilda.play();
+    if (score !== 0) {
+      banjo.play()
+      setTimeout(() => {
+        setMessageWinA(<p className="typewriter">THE PUZZLE WITH A SCORE OF {finalScore}.</p>)
+      }, 3500)
+      setTimeout(() => {
+        setMessageWinB(<p className="typewriter">HOW ABOUT TRYING TO DO IT WITH {(finalScore + 10)}?</p>)
+      }, 7500)
+    } else {
+      gruntilda.play();
+      setTimeout(() => {
+        setMessageLoseA(<p className="typewriter">WOULD BE TOO DIFFICULT FOR YOU.</p>)
+      }, 3500)
+      setTimeout(() => {
+        setMessageLoseB(<p className="typewriter">TRY AGAIN IF YOU DARE.</p>)
+      }, 7000)
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   
@@ -20,44 +42,41 @@ const EndGame = (props) => {
     refreshImages()
   }
 
-  const finalScore = () => {
-    return score * 10
-  }
+
 
   const renderMessage = () => {
     if (score !== 0) {
-      if (playerName) {
-        return (
-          <p className="typewriter">
-            GREAT JOB, {playerName}! YOU FINISHED THE PUZZLE WITH A SCORE OF {finalScore.toString()}. HOW ABOUT TRYING TO DO IT WITH {(finalScore + 10).toString()}?
-          </p>
+      return (
+        <div>
+          <p className="typewriter">GREAT JOB{playerName ? `, ${playerName}` : null}! YOU FINISHED</p>
+          {messageWinA}
+          {messageWinB}
+        </div>
         );
-      } else {
-        return (
-          <p className="typewriter">
-            GREAT JOB! YOU FINISHED THE PUZZLE WITH {score.toString()} TRIES REMAINING. HOW ABOUT TRYING TO DO IT WITH {(score + 1).toString()}?
-          </p>
-        );
-      }
     } else {
       return (
-        <p className="typewriter">HA! GAME OVER. I KNEW MY CHALLENGE WOULD BE TOO DIFFICULT FOR YOU. TRY AGAIN IF YOU DARE.</p>
+        <div>
+          <p className="typewriter">HA! GAME OVER. I KNEW MY CHALLENGE</p>
+          {messageLoseA}
+          {messageLoseB}
+        </div>
       );
     }
   }
-  
+
   return (
     <div className="end-game-left">
       <div className="end-game-left-message">
         <img src={score !== 0 ? Banjo : Gruntilda} alt="Character Speaking" />
-        {renderMessage()}
+        <div>
+          {renderMessage()}
+        </div>
       </div>
-      <h1>{score !== 0 ? `Score: ${finalScore()}` : ''}</h1>
-      <button
-        onClick={handleClick}
-        className="btn btn-warning"
-      >
-          {score === 0 ? 'RETRY' : 'PLAY AGAIN'}
+      <div className="final-score">
+        <h1>{score !== 0 ? `SCORE = ${finalScore}` : ''}</h1>
+      </div>
+      <button onClick={handleClick} className="btn btn-warning">
+        {score === 0 ? 'RETRY' : 'PLAY AGAIN'}
       </button>
     </div>
   )
