27aa403e2b94979cb7d41883ac1e06a2d5f37663
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-31 20:49:44 +0900

    2 player message

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from './images/Logo.png';
+// import Logo from './images/Logo.png';
 
 const Start = (props) => {
   const { handleStart } = props;
@@ -8,7 +8,7 @@ const Start = (props) => {
     <div className="start-screen">
       {/* <img src={Logo} alt="Logo" /> */}
       <button className="btn btn-warning" onClick={handleStart}>1 Player</button>
-      <button className="btn btn-warning" onClick={() => alert('Coming soon!')}>2 Player</button>
+      <button className="btn btn-warning" onClick={() => alert('Coming soon! N64 Expansion Pak required (not included).')}>2 Player</button>
     </div>
   )
 }
