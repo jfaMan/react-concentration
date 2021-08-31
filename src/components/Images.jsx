268f34ea870eea5ec3133611fe12dfb0a97a5c0b
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-31 19:15:45 +0900

    changed game logic to be more reactive and less DOM oriented

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -4,6 +4,7 @@ import Correct from './music/Correct.mp3';
 import Wrong from './music/Wrong.mp3';
 import Completed from './music/Completed.mp3';
 import GameOver from './music/GameOver.mp3';
+import Blank from './images/Blank.jpg';
 
 
 // Each card has either a true/false found value. If a card is found, it is NOT clickable.
@@ -15,8 +16,8 @@ import GameOver from './music/GameOver.mp3';
 const Images = (props) => {
   const { gameMusic, handleEndGame, calculateScore, score } = props;
   const [ characters, setCharacters ] = useState([])
-  const [ flippedCards, setFlippedCards ] = useState(0);
-  const [ cardPair, setCardPair ] = useState([]);
+  const [ totalFlippedCards, setTotalFlippedCards ] = useState(0);
+  const [ selectedCardIndex, setSelectedCardIndex ] = useState([]);
 
   useEffect(() => {
     setCharacters(images.sort(() => Math.random() - 0.5))
@@ -27,7 +28,7 @@ const Images = (props) => {
   useEffect(() => {
     checkEndGame()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [flippedCards])
+  }, [totalFlippedCards])
 
   useEffect(() => {
     checkEndGame()
@@ -35,50 +36,83 @@ const Images = (props) => {
   }, [score])
 
   useEffect(() => {
-    if (cardPair.length === 2) {
-      checkNames(cardPair[0], cardPair[1])
-    } else {
-      return;
-    }
+    if (selectedCardIndex.length === 2) {
+      const firstClickedIndex = selectedCardIndex[0]
+      const secondClickedIndex = selectedCardIndex[1]
+      checkNames(firstClickedIndex, secondClickedIndex)
+    } 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cardPair])
+  }, [selectedCardIndex])
 
-  const handleClick = (event) => {
-    const nextCard = event.target
-    if (nextCard.getAttribute("found") === "true") {
-      return;
-    } else {
-      nextCard.classList.remove('image-blank')
-      nextCard.setAttribute("found", "true")
-      setCardPair([...cardPair, nextCard])
+  const handleClick = (index) => {
+    const newCharacters = [...characters]
+    const clickedCard = newCharacters[index]
+    if (!clickedCard.flipped) {
+      clickedCard.flipped = true;
+      setSelectedCardIndex([...selectedCardIndex, index])
+      setCharacters(newCharacters)
     }
   }
+
+  // DOM METHOD FOR ABOVE
+  // const nextCard = event.target
+  // if (nextCard.getAttribute("found") === "true") {
+  //   return;
+  // } else {
+  //   nextCard.classList.remove('image-blank')
+  //   nextCard.setAttribute("found", "true")
+  //   setSelectedCardIndex([...selectedCardIndex, nextCard])
+  // }
   
-  const checkNames = (card1, card2) => {
-    if (card1.getAttribute("name") === card2.getAttribute("name")) {
+  const checkNames = (firstClickedIndex, secondClickedIndex) => {
+    if (characters[firstClickedIndex].name === characters[secondClickedIndex].name) {
       setTimeout(() => {
         const correct = new Audio(Correct);
         correct.play();
       }, 100)
-      setFlippedCards(flippedCards + 2)
+      setTotalFlippedCards(totalFlippedCards + 2)
     } else {
       setTimeout(() => {
         const wrong = new Audio(Wrong);
         wrong.play();
       }, 100)
       setTimeout(() => {
-        card1.setAttribute("found", "false")
-        card2.setAttribute("found", "false")
-        card1.classList.add('image-blank')
-        card2.classList.add('image-blank')
+        const newCharacters = [...characters]
+        newCharacters[firstClickedIndex].flipped = false
+        newCharacters[secondClickedIndex].flipped = false
+        setCharacters(newCharacters)
       }, 1000)
       calculateScore()
     }
-    setCardPair([])
+    setSelectedCardIndex([])
   }
+
+  // DOM METHOD FOR ABOVE
+  // const checkNames = (card1, card2) => {
+  //   if (card1.getAttribute("name") === card2.getAttribute("name")) {
+  //     setTimeout(() => {
+  //       const correct = new Audio(Correct);
+  //       correct.play();
+  //     }, 100)
+  //     setTotalFlippedCards(totalFlippedCards + 2)
+  //   } else {
+  //     setTimeout(() => {
+  //       const wrong = new Audio(Wrong);
+  //       wrong.play();
+  //     }, 100)
+  //     setTimeout(() => {
+  //       card1.setAttribute("found", "false")
+  //       card2.setAttribute("found", "false")
+  //       card1.classList.add('image-blank')
+  //       card2.classList.add('image-blank')
+  //     }, 1000)
+  //     calculateScore()
+  //   }
+  //   setSelectedCardIndex([])
+  // }
   
   const checkEndGame = () => {
-    if (flippedCards === 16 ) {
+    if (totalFlippedCards === 16 ) {
       gameMusic.volume = 0.5;
       setTimeout(() => {
         const completed = new Audio(Completed);
@@ -96,22 +130,19 @@ const Images = (props) => {
       setTimeout(() => {
         handleEndGame(true)
       }, 4000)
-    } else {
-      return;
     }
   }
 
   return (
     <div className="images">
       {characters
-        .map((image) => {
+        .map((image, index) => {
           return (
             <div
-              className="image image-blank"
+              className={characters[index].flipped ? 'image' : 'image-blank'}
               name={image.name}
-              style={{ backgroundImage: `url(${image.pic})` }}
-              found="false"
-              onClick={score === 0 || cardPair.length === 2 ? null : handleClick}
+              style={{ backgroundImage: `url(${characters[index].flipped ? image.pic : Blank})` }}
+              onClick={score === 0 || selectedCardIndex.length === 2 ? null : () => handleClick(index)}
             />
           )
         })
