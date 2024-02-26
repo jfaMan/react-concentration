3baf2c40163c620a6db53f0fff6413a15d1a02bf
Allan Szewczyk <jfa_nakata@hotmail.com>
2024-02-26 18:05:55 +0900

    Clean up navbar and refactor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,4 @@
 import NavbarLogo from './images/NavbarLogo.png';
-import React, { useState } from 'react';
-
-// import NavbarLogo2 from './images/NavbarLogo2.png';
-// import LogoToggle from './music/LogoToggle.mp3'
 
 type NavbarProps = {
   name: string;
@@ -11,21 +7,16 @@ type NavbarProps = {
 };
 
 export default function Navbar({ name, score, start }: NavbarProps) {
-  // const [ oldLogo, setOldLogo] = useState(false);
 
-  // const toggleLogo = () => {
-  //   setOldLogo(!oldLogo)
-  //   const toggleJingle = new Audio(LogoToggle);
-  //   toggleJingle.play();
-  // }
   return (
     <div className="navbar">
       <img
+        className="navbar__logo"
         src={NavbarLogo}
         alt="Logo"
       />
-      {start && <div className="score">TRIES REMAINING: {score}</div>}
-      {start && <div className="name">NAME: {name}</div>}
+      {start && <div className="navbar__score">TRIES REMAINING: {score}</div>}
+      {start && <div className="navbar__name">NAME: {name}</div>}
     </div>
   );
 };
